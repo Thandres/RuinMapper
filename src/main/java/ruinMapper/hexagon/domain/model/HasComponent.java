1d1d158ee4a8a966c9d3be24ddb560743a987b9c
Arno Gabler <arno.gabler@hotmail.com>
2019-05-05 19:13:24 +0200

    updated description

--- src/main/java/ruinMapper/hexagon/domain/model/HasComponent.java
+++ src/main/java/ruinMapper/hexagon/domain/model/HasComponent.java
@@ -2,6 +2,8 @@ package ruinMapper.hexagon.domain.model;
 
 /**
  * Marker interface used to lessen invariant complexity.
+ * Indicates the type of Manager the implementing class uses to access
+ * the components
  */
 public interface HasComponent {
     public ComponentType getType();
